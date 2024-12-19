32281e09d1bfb92a2986e2afb6eb793f00b7df08
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-19 11:20:13 +0100

    Add FitnessMethods

---
+++ FitnessMethods.py
@@ -0,0 +1,16 @@
+from protocols.IndividualProtocol import IndividualProtocol
+
+
+class FitnessMethods:
+	@staticmethod
+	def negative_of_length(individual: IndividualProtocol):
+		"""
+		Calculate the fitness of the individual as the negative of the length of the cycle.
+		:param individual: The individual
+		"""
+		distance_matrix = individual.distance_matrix
+		cycle = individual.cycle
+		length = 0
+		for i in range(len(cycle)):
+			length += distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]]
+		return -length
--- protocols/IndividualProtocol.py
+++ protocols/IndividualProtocol.py
@@ -3,19 +3,16 @@ from custom_types import DistanceMatrix, Cycle
 
 
 class IndividualProtocol(Protocol):
-    cycle: Cycle
-    distance_matrix: DistanceMatrix
-    _fitness: float
+	cycle: Cycle
+	distance_matrix: DistanceMatrix
+	_fitness: float
 
-    @property
-    def fitness(self) -> float:
-        ...
+	@property
+	def fitness(self) -> float:
+		...
 
-    def calculate_fitness(self) -> float:
-        ...
+	def mutate(self) -> None:
+		...
 
-    def mutate(self) -> None:
-        ...
-
-    def __str__(self) -> str:
-        ...
+	def __str__(self) -> str:
+		...
