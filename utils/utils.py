fedb1d9f6a8f8e6ba5ac477a66dd1ba7446a7bc7
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-24 17:55:47 +0100

    fix: normalize util

--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,7 @@ def normalize_list(values: List[float], bottom: int = 0, top: int = 100) -> List
 	if range_val == 0:
 		return [100.0 for _ in values]  # All values are the same
 
-	# Normalize values to [0, 1] and then scale to [0, 100]
-	normalized = [((x - min_val) / range_val * top) + bottom for x in values]
+	# Normalize values to [0, 1] and then scale to [bottom, top]
+	normalized_range = top - bottom
+	normalized = [((x - min_val) / range_val * normalized_range) + bottom for x in values]
 	return normalized
