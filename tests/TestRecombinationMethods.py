48391b848e1ed5a3872f190af8ba3e7ffb857ca8
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-24 11:11:39 +0100

    Add test_deterministic_best_parent

--- tests/TestRecombinationMethods.py
+++ tests/TestRecombinationMethods.py
@@ -1,10 +1,24 @@
 import unittest
+from unittest.mock import patch
+
+import numpy as np
+
+from classes.DistanceMatrix import DistanceMatrix
+from tests.MockIndividual import MockIndividual
+
+from methods.RecombinationMethods import RecombinationMethods
+
+MOCK_DISTANCE_MATRIX = DistanceMatrix(np.array([1, 1, 1, 1, 1, 1,
+												1, 1, 1, 1, 1, 1,
+												1, 1, 1, 1, 1, 1]))
+MOCK_INDIVIDUAL1 = MockIndividual(np.array([1, 2, 3, 6, 5, 4]), MOCK_DISTANCE_MATRIX, lambda _: 5, lambda _: None)
+MOCK_INDIVIDUAL2 = MockIndividual(np.array([4, 5, 3, 2, 1, 6]), MOCK_DISTANCE_MATRIX, lambda _: 1, lambda _: None)
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+class TestRecombinationMethods(unittest.TestCase):
+	def test_deterministic_best_parent(self, ):
+		offspring = RecombinationMethods.deterministic_best_parent(MOCK_INDIVIDUAL1, MOCK_INDIVIDUAL2)
+		self.assertEqual(offspring, MOCK_INDIVIDUAL1)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
