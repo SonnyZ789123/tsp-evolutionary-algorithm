2fd0bc297361c16bea7a92595de32b613130dbf6
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-24 16:45:30 +0100

    Add plots

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from typing import List
+
 import numpy as np
 
 from classes.DistanceMatrix import DistanceMatrix
 from classes.EvolutionaryAlgorithm import EvolutionaryAlgorithm
 from protocols.DistanceMatrixProtocol import DistanceMatrixProtocol
 from protocols.EvolutionaryAlgorithmProtocol import EvolutionaryAlgorithmProtocol
+from utils.plotting import generate_plot, normalize_to_0_100
 
 
 def solve_tsp():
@@ -18,15 +21,21 @@ def solve_tsp():
 	assert distance_matrix.value.shape[0] == distance_matrix.value.shape[1]  # distance_matrix should be square
 	evolutionary_algorithm: EvolutionaryAlgorithmProtocol = EvolutionaryAlgorithm(distance_matrix)
 
+	mean_fitness_history: List[float] = []
+	best_fitness_history: List[float] = []
+
 	while not evolutionary_algorithm.converged:
 		evolutionary_algorithm.select()
 		evolutionary_algorithm.recombination()
 		evolutionary_algorithm.mutation()
 		evolutionary_algorithm.elimination()
 
-		print("mean fitness", evolutionary_algorithm.population.mean_fitness())
-		print("best fitness", evolutionary_algorithm.population.best_fitness())
-		print("best individual", evolutionary_algorithm.population.best_individual().cycle)
+		# For plotting
+		mean_fitness_history.append(evolutionary_algorithm.population.mean_fitness())
+		best_fitness_history.append(evolutionary_algorithm.population.best_fitness())
+
+	generate_plot(list(range(len(mean_fitness_history))), normalize_to_0_100(mean_fitness_history), y_label="Mean fitness")
+	generate_plot(list(range(len(best_fitness_history))), normalize_to_0_100(best_fitness_history), y_label="Best fitness")
 
 	return 0
 
