2195b82125075671fbc31b882fa5d3758f4929c4
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-24 16:56:42 +0100

    Add additional test

--- tests/TestFitnessMethods.py
+++ tests/TestFitnessMethods.py
@@ -10,14 +10,19 @@ from methods.FitnessMethods import FitnessMethods
 MOCK_DISTANCE_MATRIX = DistanceMatrix(np.array([[1, 2, 1],
 												[9, 3, 2],
 												[1, 5, 4]]))
-MOCK_INDIVIDUAL = MockIndividual(np.array([1, 0, 2]), MOCK_DISTANCE_MATRIX, lambda _: -1, lambda _: None)
+MOCK_INDIVIDUAL1 = MockIndividual(np.array([1, 0, 2]), MOCK_DISTANCE_MATRIX, lambda _: -1, lambda _: None)
+MOCK_INDIVIDUAL2 = MockIndividual(np.array([0, 1, 2]), MOCK_DISTANCE_MATRIX, lambda _: -1, lambda _: None)
 
 
 class TestFitnessMethods(unittest.TestCase):
 	def test_negative_of_length(self):
-		fitness = FitnessMethods.negative_of_length(MOCK_INDIVIDUAL)
-		expected_fitness = -(9 + 1 + 5)
-		self.assertEqual(expected_fitness, fitness)
+		fitness1 = FitnessMethods.negative_of_length(MOCK_INDIVIDUAL1)
+		expected_fitness1 = -(9 + 1 + 5)
+		self.assertEqual(expected_fitness1, fitness1)
+
+		fitness2 = FitnessMethods.negative_of_length(MOCK_INDIVIDUAL2)	# -(2 + 2 + 1) = -5
+		self.assertGreater(fitness2, fitness1)
+
 
 
 if __name__ == '__main__':
