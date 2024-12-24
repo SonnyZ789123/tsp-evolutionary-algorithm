e521963de300b6cb9cb952c336e13337957c04fb
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-24 11:33:31 +0100

    Add documentation

--- protocols/IndividualProtocol.py
+++ protocols/IndividualProtocol.py
@@ -5,11 +5,17 @@ from protocols.DistanceMatrixProtocol import DistanceMatrixProtocol
 
 class IndividualProtocol(Protocol):
 	cycle: Cycle
+	""" The cycle of the individual, represented as a list of indices, from 1 to length of the distance matrix. """
 	distance_matrix: DistanceMatrixProtocol
+	""" 
+	The distance matrix representing the distances between the cities. The length of the rows should be equal 
+	to the length of the columns, and the length should be equal to the length of the cycle. 
+	"""
 	_fitness: float
 
 	@property
 	def fitness(self) -> float:
+		""" The fitness of the individual, which can be negative. """
 		...
 
 	def mutate(self) -> None:
