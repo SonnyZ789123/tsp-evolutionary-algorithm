71356482f54f577f7635ec520275eaf5af9f02cc
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-26 17:44:25 +0100

    Move main loop to r0829897

--- main.py
+++ main.py
@@ -1,69 +1,9 @@
-import time
-from typing import List
-
-import numpy as np
-
-from config.custom_types import DistanceMatrix
-from classes.EvolutionaryAlgorithm import EvolutionaryAlgorithm
-from protocols.EvolutionaryAlgorithmProtocol import EvolutionaryAlgorithmProtocol
-from utils.cycle_utils import get_cycle_length, get_cycle_distances
-from utils.plotting import generate_plot, generate_log_plot
+from r0829897 import r0829897
 
 
-def solve_tsp():
-	# Read distance matrix from file.
-	tour_name = "tour1000"
-	file = open(f"{tour_name}.csv")
-	distance_matrix: DistanceMatrix = np.loadtxt(file, delimiter=",")
-	file.close()
-
-	assert distance_matrix.ndim == 2  # distance_matrix should be a 2D array
-	assert distance_matrix.shape[0] == distance_matrix.shape[1]  # distance_matrix should be square
-	evolutionary_algorithm: EvolutionaryAlgorithmProtocol = EvolutionaryAlgorithm(distance_matrix)
-
-	mean_fitness_history: List[float] = []
-	best_fitness_history: List[float] = []
-	variance_fitness_history: List[float] = []
-
-	# Time the algorithm
-	start_time = time.time()
-
-	while not evolutionary_algorithm.converged:
-		evolutionary_algorithm.select()
-		evolutionary_algorithm.recombination()
-		evolutionary_algorithm.mutation()
-		evolutionary_algorithm.elimination()
-
-		# For plotting
-		mean_fitness_history.append(evolutionary_algorithm.population.mean_fitness())
-		best_fitness_history.append(evolutionary_algorithm.population.best_fitness())
-		variance_fitness_history.append(evolutionary_algorithm.population.variance_fitness())
-		_, invalid_cycles = evolutionary_algorithm.population.valid_invalid_individual_proportion()
-		if invalid_cycles > 0:
-			print(f"Invalid cycles found: {invalid_cycles}")
-
-	end_time = time.time()
-	elapsed_time = end_time - start_time
-	print(f"Elapsed time for solving TSP: {elapsed_time:.2f} seconds")
-
-	# Plotting
-	iteration_numbers = list(range(len(mean_fitness_history)))
-	mean_fitness_history_rebased = [(i / 1000 + 110) for i in mean_fitness_history]
-	best_fitness_history_rebased = [(i / 1000 + 110) for i in best_fitness_history]
-	generate_plot(iteration_numbers, mean_fitness_history_rebased, y_label="Mean fitness/1000 + 110",
-				  title=f"Mean fitness history for {tour_name}")
-	generate_plot(iteration_numbers, best_fitness_history_rebased, y_label="Best fitness/1000 + 110",
-				  title=f"Best fitness history for {tour_name}")
-	generate_log_plot(iteration_numbers, variance_fitness_history, y_label="Variance fitness",
-					  title=f"Variance fitness history for {tour_name}")
-	print(f"Best individual cycle length: {
-	get_cycle_length(evolutionary_algorithm.population.best_individual().cycle, distance_matrix):.0f}")
-	# print(f"Best individual cycle: {evolutionary_algorithm.population.best_individual().cycle}")
-	# print(f"Best individual cycle distances: {
-	# get_cycle_distances(evolutionary_algorithm.population.best_individual().cycle, distance_matrix)}")
-
-	return 0
-
+def main():
+	solve_tsp = r0829897()
+	solve_tsp.optimize("tour500.csv")
 
 if __name__ == "__main__":
-	solve_tsp()
+	main()
--- r0829897.py
+++ r0829897.py
@@ -1,6 +1,14 @@
+import time
+from typing import List
+
 import numpy as np
 
 import Reporter
+from classes.EvolutionaryAlgorithm import EvolutionaryAlgorithm
+from config.custom_types import DistanceMatrix
+from protocols.EvolutionaryAlgorithmProtocol import EvolutionaryAlgorithmProtocol
+from utils.cycle_utils import get_cycle_length
+from utils.plotting import generate_plot, generate_log_plot
 
 
 # Modify the class name to match your student number.
@@ -11,28 +19,71 @@ class r0829897:
 
 	# The evolutionary algorithm's main loop
 	def optimize(self, filename):
+		################################################### from the template
 		# Read distance matrix from file.
 		file = open(filename)
-		distanceMatrix = np.loadtxt(file, delimiter=",")
+		distance_matrix: DistanceMatrix = np.loadtxt(file, delimiter=",")
 		file.close()
+		###################################################
 
-		# TODO: Your code here.
-		yourConvergenceTestsHere = True
-		while (yourConvergenceTestsHere):
-			meanObjective = 0.0
-			bestObjective = 0.0
-			bestSolution = np.array([1, 2, 3, 4, 5])
+		assert distance_matrix.ndim == 2  # distance_matrix should be a 2D array
+		assert distance_matrix.shape[0] == distance_matrix.shape[1]  # distance_matrix should be square
+		evolutionary_algorithm: EvolutionaryAlgorithmProtocol = EvolutionaryAlgorithm(distance_matrix)
 
-			# Your code here.
+		mean_fitness_history: List[float] = []
+		best_fitness_history: List[float] = []
+		variance_fitness_history: List[float] = []
 
-			# Call the reporter with:
+		# Time the algorithm
+		start_time = time.time()
+		while not evolutionary_algorithm.converged:
+			evolutionary_algorithm.select()
+			evolutionary_algorithm.recombination()
+			evolutionary_algorithm.mutation()
+			evolutionary_algorithm.elimination()
+
+			mean_fitness = evolutionary_algorithm.population.mean_fitness()
+			best_fitness = evolutionary_algorithm.population.best_fitness()
+			variance_fitness = evolutionary_algorithm.population.variance_fitness()
+			best_solution = evolutionary_algorithm.population.best_individual().cycle
+
+			# For plotting
+			mean_fitness_history.append(mean_fitness)
+			best_fitness_history.append(best_fitness)
+			variance_fitness_history.append(variance_fitness)
+			_, invalid_cycles = evolutionary_algorithm.population.valid_invalid_individual_proportion()
+			if invalid_cycles > 0:
+				print(f"Invalid cycles found: {invalid_cycles}")
+
+			###################################################
+			# Call the reporter with: KEEP THIS
 			#  - the mean objective function value of the population
 			#  - the best objective function value of the population
 			#  - a 1D numpy array in the cycle notation containing the best solution
 			#    with city numbering starting from 0
-			timeLeft = self.reporter.report(meanObjective, bestObjective, bestSolution)
+			timeLeft = self.reporter.report(mean_fitness, best_fitness, best_solution)
 			if timeLeft < 0:
 				break
+			###################################################
 
-		# TODO: Your code here.
+		end_time = time.time()
+		elapsed_time = end_time - start_time
+		print(f"Elapsed time for solving TSP: {elapsed_time:.2f} seconds")
+
+		# Plotting
+		iteration_numbers = list(range(len(mean_fitness_history)))
+		mean_fitness_history_rebased = [(i / 1000 + 110) for i in mean_fitness_history]
+		best_fitness_history_rebased = [(i / 1000 + 110) for i in best_fitness_history]
+		generate_plot(iteration_numbers, mean_fitness_history_rebased, y_label="Mean fitness/1000 + 110",
+					  title=f"Mean fitness history for {filename}")
+		generate_plot(iteration_numbers, best_fitness_history_rebased, y_label="Best fitness/1000 + 110",
+					  title=f"Best fitness history for {filename}")
+		generate_log_plot(iteration_numbers, variance_fitness_history, y_label="Variance fitness",
+						  title=f"Variance fitness history for {filename}")
+		print(f"Best individual cycle length: {
+		get_cycle_length(evolutionary_algorithm.population.best_individual().cycle, distance_matrix):.0f}")
+		# print(f"Best individual cycle: {evolutionary_algorithm.population.best_individual().cycle}")
+		# print(f"Best individual cycle distances: {
+		# get_cycle_distances(evolutionary_algorithm.population.best_individual().cycle, distance_matrix)}")
+
 		return 0
