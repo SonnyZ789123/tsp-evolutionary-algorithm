0e2f516b3f8d3e95f41247da9edeeef9b56d6c23
SonnyZ789123 <yoran.mertens@telenet.be>
2024-12-28 09:27:12 +0100

    Add scramble mutation

--- methods/MutationMethods.py
+++ methods/MutationMethods.py
@@ -21,12 +21,34 @@ class MutationMethods:
 		:param individual: The individual
 		"""
 		distance_matrix = individual.distance_matrix
-		i, j = np.random.choice(len(individual.cycle), 2, replace=False)
+		# list[i:j] returns a list from i to and including j-1
+		# np.random.choice(cycle_length) returns a random number from 0 to size-1, but we want including cycle_length
+		i, j = np.random.choice(len(individual.cycle) + 1, 2, replace=False)
 		if i > j:
 			i, j = j, i
 
 		# Check if the cycle is valid
 		mutated_cycle = individual.cycle.copy()
 		mutated_cycle[i:j] = mutated_cycle[i:j][::-1]
+		if is_valid_cycle(mutated_cycle, distance_matrix):
+			individual.cycle = mutated_cycle
+
+	@staticmethod
+	def scramble(individual: IndividualProtocol) -> None:
+		"""
+		A random part of the cycle is selected and its elements are randomly shuffled. If the resulting cycle is invalid,
+		it is not changed.
+		:param individual: The individual
+		"""
+		distance_matrix = individual.distance_matrix
+		# list[i:j] returns a list from i to and including j-1
+		# np.random.choice(cycle_length) returns a random number from 0 to size-1, but we want including cycle_length
+		i, j = np.random.choice(len(individual.cycle) + 1, 2, replace=False)
+		if i > j:
+			i, j = j, i
+
+		# Check if the cycle is valid
+		mutated_cycle = individual.cycle.copy()
+		mutated_cycle[i:j] = np.random.permutation(mutated_cycle[i:j])
 		if is_valid_cycle(mutated_cycle, distance_matrix):
 			individual.cycle = mutated_cycle
